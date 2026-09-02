d48a4d3f5449798f596fd21960eb5e5f60577472
Diego-lz <272330@unifio.edu.br>
2026-09-02 22:47:03 +0000

    ex10

--- primeiro-projeto/src/app/exercicios/exercicios-routing-module.ts
+++ primeiro-projeto/src/app/exercicios/exercicios-routing-module.ts
@@ -9,6 +9,7 @@ import { Ex6 } from './ex6/ex6';
 import { Ex7 } from './ex7/ex7';
 import { Ex8 } from './ex8/ex8';
 import { Ex9 } from './ex9/ex9';
+import { Ex10 } from './ex10/ex10';
 
 
 const routes: Routes = [
@@ -21,6 +22,7 @@ const routes: Routes = [
  { path: 'exercicio7', component: Ex7 },
  { path: 'exercicio8', component: Ex8 },
  { path: 'exercicio9', component: Ex9 },
+ { path: 'exercicio10', component: Ex10 },
  
 ];
 
