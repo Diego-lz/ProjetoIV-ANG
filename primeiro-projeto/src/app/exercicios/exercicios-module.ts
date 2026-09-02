43ed95e3affbf2de9e68de2eb375832938ccc2f4
ProjetoIV-DL <projetoivdiego@gmail.com>
2026-09-02 01:30:05 +0000

    ex3

--- primeiro-projeto/src/app/exercicios/exercicios-module.ts
+++ primeiro-projeto/src/app/exercicios/exercicios-module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Ex1 } from './ex1/ex1';
 import { ExerciciosRoutingModule } from './exercicios-routing-module';
+import { Ex2 } from './ex2/ex2';
 
 @NgModule({
-  declarations: [Ex1],
+  declarations: [Ex1, Ex2],
   imports: [CommonModule, FormsModule, ExerciciosRoutingModule],
 })
 export class ExerciciosModule {}
--- primeiro-projeto/src/app/exercicios/exercicios-routing-module.ts
+++ primeiro-projeto/src/app/exercicios/exercicios-routing-module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Ex1 } from './ex1/ex1';
+import { Ex2 } from './ex2/ex2';
 
 
 const routes: Routes = [
- { path: 'exercicio1', component: Ex1 }
+ { path: 'exercicio1', component: Ex1 },
+ { path: 'exercicio2', component: Ex2 }
 ];
 
 @NgModule({
