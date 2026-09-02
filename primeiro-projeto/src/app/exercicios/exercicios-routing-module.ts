ab35094bc490c9689772fb327621890796a95b37
ProjetoIV-DL <projetoivdiego@gmail.com>
2026-09-02 03:37:54 +0000

    ex5

--- primeiro-projeto/src/app/exercicios/exercicios-routing-module.ts
+++ primeiro-projeto/src/app/exercicios/exercicios-routing-module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { Ex1 } from './ex1/ex1';
 import { Ex2 } from './ex2/ex2';
 import { Ex3 } from './ex3/ex3';
+import { Ex4 } from './ex4/ex4';
 
 
 const routes: Routes = [
  { path: 'exercicio1', component: Ex1 },
  { path: 'exercicio2', component: Ex2 },
  { path: 'exercicio3', component: Ex3 },
+ { path: 'exercicio4', component: Ex4 },
  
 ];
 
