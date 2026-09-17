6b4a874245dd2a0ab1a1ce5e03f9f61ce9ea6436
Diego-lz <272330@unifio.edu.br>
2026-09-17 18:59:08 +0000

    exConcluido

--- primeiro-projeto/src/app/exercicios2/exercicios2-module.ts
+++ primeiro-projeto/src/app/exercicios2/exercicios2-module.ts
@@ -14,9 +14,12 @@ import { Ex9 } from './ex9/ex9';
 import { Ex10 } from './ex10/ex10';
 import { Ex11 } from './ex11/ex11';
 import { Ex12 } from './ex12/ex12';
+import { Ex13 } from './ex13/ex13';
+import { Ex14 } from './ex14/ex14';
+import { Ex15 } from './ex15/ex15';
 
 @NgModule({
-  declarations: [Ex1, Ex2, Ex3, Ex4, Ex5, Ex6, Ex7, Ex8, Ex9, Ex10, Ex11, Ex12],
+  declarations: [Ex1, Ex2, Ex3, Ex4, Ex5, Ex6, Ex7, Ex8, Ex9, Ex10, Ex11, Ex12, Ex13, Ex14, Ex15],
   imports: [CommonModule, FormsModule, Exercicios2RoutingModule],
 })
 export class Exercicios2Module {}
--- primeiro-projeto/src/app/exercicios2/exercicios2-routing-module.ts
+++ primeiro-projeto/src/app/exercicios2/exercicios2-routing-module.ts
@@ -12,6 +12,10 @@ import { Ex9 } from './ex9/ex9';
 import { Ex10 } from './ex10/ex10';
 import { Ex11 } from './ex11/ex11';
 import { Ex12 } from './ex12/ex12';
+import { Ex13 } from './ex13/ex13';
+import { Ex14 } from './ex14/ex14';
+import { Ex15 } from './ex15/ex15';
+
 
 const routes: Routes = [
   { path: 'ex1', component: Ex1 },
@@ -26,6 +30,9 @@ const routes: Routes = [
   { path: 'ex10', component: Ex10 },
   { path: 'ex11', component: Ex11 },
   { path: 'ex12', component: Ex12 },
+  { path: 'ex13', component: Ex13 },
+  { path: 'ex14', component: Ex14 },
+  { path: 'ex15', component: Ex15 }
 ];
 
 @NgModule({
